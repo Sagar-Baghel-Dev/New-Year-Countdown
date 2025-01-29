5006a2531aeeb1fa375fe52d24e0063a687078c3
sagar <sagarbaghel.developer@gmail.com>
2025-01-29 11:17:28 +0530

    added expired feature

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function dateTime(){
   let countDownDate = new Date(document.getElementById("date").value).getTime();
-
+ 
+    
 let x = setInterval(()=>{
     let now = new Date().getTime();
     let distance = countDownDate - now ;
@@ -9,25 +10,30 @@ let x = setInterval(()=>{
     let minutes = document.getElementById("mm");
     let seconds = document.getElementById("ss");
 
-    let d =0;
-    let h=0;
-    let m=0;
-    let s=0;
-    
+  
 
-     d = Math.floor(distance / (1000 * 60 * 60 * 24));
-     h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-     m =  Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-     s = Math.floor((distance % (1000 * 60)) / 1000);
+    let  d = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let  h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let  m =  Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let  s = Math.floor((distance % (1000 * 60)) / 1000);
 
     days.innerHTML = d;
     hours.innerHTML = h;
     minutes.innerHTML = m;
     seconds.innerHTML = s;
 
-    if (distance < 0) {
+    if (distance <= 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        document.getElementById("dd").innerHTML = "EX";
+        document.getElementById("hh").innerHTML = "PI";
+        document.getElementById("mm").innerHTML = "RE";
+        document.getElementById("ss").innerHTML = "D";
+        console.log("expired");
       }
 }, 1000);
+
+d =0;
+h=0;
+m=0;
+s=0;
 }
